a0c292f82c72fd5db2bacfccc725a9bda50f3969
aswin0207 <aswinviruthiyil@gmail.com>
2023-03-15 12:34:31 +0530

    update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -9,31 +9,35 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  acno:any
-  uname:any
-  psw:any
+  // acno:any
+  // uname:any
+  // psw:any
 
   constructor (private ds:DataService,private router:Router,private fb:FormBuilder){ }
 
 // model for register form
 registerForm=this.fb.group({
-  acno:[''],
-  psw:[''],
-  uname:['']
+  acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+  psw:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]+')]],
+  uname:['',[Validators.required,Validators.pattern('[a-zA-Z]+')]]
 })
 
   register(){
-    var acno=this.acno
-    var uname=this.uname
-    var psw=this.psw
-
-const result=this.ds.register(acno,uname,psw)
-if(result){
-alert("registration sucessfull")
-}
-else{
-  alert("user already exist")
-}
-  }
-
+    var acno=this.registerForm.value.acno
+    var uname=this.registerForm.value.uname
+    var psw=this.registerForm.value.psw
+    if(this.registerForm.valid){
+      const result=this.ds.register(acno,uname,psw)
+      if(result){
+      alert("registration sucessfull")
+      this.router.navigateByUrl("")
+      }
+      else{
+        alert("user already exist")
+      }
+        }
+        else{
+          alert('invalid form')
+        }
+      }
 }
